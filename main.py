daf80f529bb13e12ea7e09552b3f1945d8c40cf5
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-10 21:28:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+import spacy
+import streamlit as st
+from spacy import displacy
+from spacy.tokenizer import Tokenizer
+
+from utils import *
+
+nlp = spacy.load('pt_core_news_sm')
+st.title(title_msg)
+
+col1, col2, col3 = st.columns(3)
+
+with st.sidebar:
+	st.markdown("## Objetivo")
+	st.write(desc_app)
+	st.markdown("## Sumário")
+	st.markdown(""" 
+	
+	- Tokenização
+	- Pos (Parts Of Speach)
+	- Contador de Caracteres
+	- Lemmanization
+	
+	""")
+	
+	st.markdown("---")
+	st.markdown(f"Feito por [CLL](https://github.com/CllsPy)")
+	
+def explain(text_input: str) -> str:
+	
+	answer  = []
+	doc = nlp(text_input)
+
+	for token in doc:
+		answer.append((token.text, token.pos_,
+			 spacy.explain(token.pos_)))
+
+	return answer
+
+with st.expander('Tokenização'):
+	st.markdown(token_msg)
+	
+	st.info("Por favor insira seu texto abaixo")
+
+	token_msg_ = st.text_area('Tokenizar')
+	tokenizer_answer_ = nlp(token_msg_)
+	st.button("Tokenizar", type="primary")
+
+	if st.button:
+		st.markdown(([w.text for w in tokenizer_answer_]))
+	
+with st.expander('Parte da Fala (POS)'):
+	st.markdown(pos_msg)
+	st.info("Por favor insira seu texto abaixo")
+	msg = st.text_area('POS')
+	st.button("Parts Of Speach", type="primary")
+
+	if st.button:
+		st.markdown(explain(msg))
+
+with col1:
+	with st.expander("Contador de Caracteres"):
+		st.info("Por favor insira seu texto abaixo")
+		text = st.text_area("Seu Texto")
+		st.button("Contar", type="primary")
+	
+		if st.button:
+			st.markdown(f'Você escreveu {len(text)} caracteres!')
+	
+
+with st.expander('Lemmanization'):
+	st.markdown(lem_msg)
+	st.info("Por favor insira seu texto abaixo")
+	answer_lemm = []
+	text_lem = st.text_area("Lemma")
+	
+	for token in nlp(text_lem): 
+	  lemma = token.lemma_ 
+	  answer_lemm.append((token.text, "-->", lemma))
+	
+	st.button("Lemmanitizar", type="primary")
+
+	if st.button:
+		st.markdown(answer_lemm)
+	
+
+	
+
+
---
+++ utils.py
@@ -0,0 +1,9 @@
+token_msg = "Tokenização é o processo de dividir texto em unidades menores (tokens), facilitando o processamento NLP."
+
+title_msg = "NLP Playgorund"
+
+pos_msg = "POS (Part of Speech) são categorias gramaticais que indicam a função das palavras na frase."
+
+lem_msg = "Lemmatization é o processo de reduzir uma palavra à sua forma base ou lema, considerando o contexto gramatical."
+
+desc_app = "App em Python e spaCy para explorar tokenização, lematização e POS em textos de forma simples."
